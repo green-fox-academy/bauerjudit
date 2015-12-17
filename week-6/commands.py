78520b84444b5546d9e2517f07ee39d29166da6a
Judit Bauer <judit.bauer@yahoo.com>
2015-12-17 14:36:33 +0100

    add Actions class

--- week-6/commands.py
+++ week-6/commands.py
@@ -13,16 +13,13 @@ class MenuItem():
 class Menu():
     def __init__(self, items):
         self.items = items
-
     def choose (self, number):
         for item in self.items:
             if item.num == number:
                 return item.action()
-
     def print_menu(self):
         for item in self.items:
             print(item)
-
     def user_input(self):
         while True:
             try:
@@ -33,12 +30,9 @@ class Menu():
                     return user_input
             except ValueError:
                 print("Wrong input")
-
-
     def print_input_choose(self):
         self.print_menu()
         self.choose(self.user_input())
-
     def exit(self):
         pass
 
@@ -62,7 +56,7 @@ class Character():
         return(print("dexterity:", self.dexterity, "health:", self.health, "luck:", self.luck))
 
     def choose_potion(self, potion):
-        reselect_potion_action(potion)
+        action.reselect_potion_action(potion)
 
     def inventory(self):
         return(print("Character Name:", self.name, "Dexterity:", self.dexterity, "Health:", self.health, "Luck:", self.luck, "Potion:", self.potion))
@@ -122,103 +116,89 @@ class Strike():
             new_player.luck -= 1
             print("Health:", new_player.health, "Luck:", new_player.luck)
 
-
-def try_your_luck_action():
-    fight.try_your_luck_method()
-
-
-def after_strike_action():
-    begin_items = Menu([
-        MenuItem(1, "Strike", strike_submenu_action),
-        MenuItem(2, "Retreat", None),
-        MenuItem(3, "Quit", exit),
-    ])
-    monster.random_health_enemy()
-    fight.after_strike_method()
-    begin_items.print_input_choose()
-
-
-def strike_submenu_action():
-    strike_submenu_items = Menu([
-        MenuItem(1, "Continue", after_strike_action),
-        MenuItem(2, "Try your Luck", try_your_luck_action),
-        MenuItem(3, "Retreat", None),
-        MenuItem(4, "Quit", exit)
-    ])
-
-    fight.strike_method()
-    strike_submenu_items.print_input_choose()
-
-def begin_action():
-    begin_items = Menu([
-        MenuItem(1, "Strike", strike_submenu_action),
-        MenuItem(2, "Retreat", None),
-        MenuItem(3, "Quit", exit),
-    ])
-    new_player.inventory()
-    monster.random_dexterity_enemy()
-    monster.random_health_enemy()
-    monster.enemy_inventory()
-    begin_items.print_input_choose()
-
-
-def reroll_status_action():
-    new_player.random_dexterity_health_luck()
-    continue_action()
-
-def reselect_potion_action(potion):
-    reselect_potion_items = Menu([
-        MenuItem(1, "Reselect Potion", potion_menu_action),
-        MenuItem(2, "Continue", begin_action),
-        MenuItem(3, "Quit", exit),
-    ])
-    print(potion)
-    reselect_potion_items.print_input_choose()
-
-def potion_menu_action():
-    potion_menu_items = Menu([
-        MenuItem(1, "Potion of Health", lambda : new_player.choose_potion("Potion of Health")),
-        MenuItem(2, "Potion of Dexterity", lambda : new_player.choose_potion("Potion of Dexterity")),
-        MenuItem(3, "Potion of Luck", lambda: new_player.choose_potion("Potion of Luck")),
-    ])
-    potion_menu_items.print_input_choose()
-
-
-def continue_action():
-    continue_items = Menu([
-        MenuItem(1, "Reroll status", reroll_status_action),
-        MenuItem(2, "Continue", potion_menu_action),
-        MenuItem(3, "Save", None),
-        MenuItem(4, "Quit", exit)
-    ])
-    new_player.random_dexterity_health_luck()
-    continue_items.print_input_choose()
-
-def quit_action():
-    quit_items = Menu([
-        MenuItem(1, "Save and quit", None),
-        MenuItem(2, "Quit and not save", exit),
-        MenuItem(3, "Resume", None)
-    ])
-    quit_items.print_input_choose()
-
-
-def new_game_action():
-    new_game_items = Menu([
-        MenuItem(1, "Reenter name", new_game_action),
-        MenuItem(2, "Continue", continue_action),
-        MenuItem(3, "Save", None),
-        MenuItem(4, "Quit", quit_action),
+class Actions():
+    def try_your_luck_action(self):
+        fight.try_your_luck_method()
+    def after_strike_action(self):
+        begin_items = Menu([
+            MenuItem(1, "Strike", action.strike_submenu_action),
+            MenuItem(2, "Retreat", None),
+            MenuItem(3, "Quit", exit),
         ])
-    new_player.add_character_name()
-    new_game_items.print_input_choose()
+        monster.random_health_enemy()
+        fight.after_strike_method()
+        begin_items.print_input_choose()
+    def strike_submenu_action(self):
+        strike_submenu_items = Menu([
+            MenuItem(1, "Continue", action.after_strike_action),
+            MenuItem(2, "Try your Luck", action.try_your_luck_action),
+            MenuItem(3, "Retreat", None),
+            MenuItem(4, "Quit", exit)
+        ])
+        fight.strike_method()
+        strike_submenu_items.print_input_choose()
+    def begin_action(self):
+        begin_items = Menu([
+            MenuItem(1, "Strike", action.strike_submenu_action),
+            MenuItem(2, "Retreat", None),
+            MenuItem(3, "Quit", exit),
+        ])
+        new_player.inventory()
+        monster.random_dexterity_enemy()
+        monster.random_health_enemy()
+        monster.enemy_inventory()
+        begin_items.print_input_choose()
+    def reroll_status_action(self):
+        new_player.random_dexterity_health_luck()
+        action.continue_action()
+    def reselect_potion_action(self, potion):
+        reselect_potion_items = Menu([
+            MenuItem(1, "Reselect Potion", action.potion_menu_action),
+            MenuItem(2, "Continue", action.begin_action),
+            MenuItem(3, "Quit", exit),
+        ])
+        print(potion)
+        reselect_potion_items.print_input_choose()
+    def potion_menu_action(self):
+        potion_menu_items = Menu([
+            MenuItem(1, "Potion of Health", lambda : new_player.choose_potion("Potion of Health")),
+            MenuItem(2, "Potion of Dexterity", lambda : new_player.choose_potion("Potion of Dexterity")),
+            MenuItem(3, "Potion of Luck", lambda: new_player.choose_potion("Potion of Luck")),
+        ])
+        potion_menu_items.print_input_choose()
+    def continue_action(self):
+        continue_items = Menu([
+            MenuItem(1, "Reroll status", action.reroll_status_action),
+            MenuItem(2, "Continue", action.potion_menu_action),
+            MenuItem(3, "Save", None),
+            MenuItem(4, "Quit", exit)
+        ])
+        new_player.random_dexterity_health_luck()
+        continue_items.print_input_choose()
+    def quit_action(self):
+        quit_items = Menu([
+            MenuItem(1, "Save and quit", None),
+            MenuItem(2, "Quit and not save", exit),
+            MenuItem(3, "Resume", None)
+        ])
+        quit_items.print_input_choose()
+    def new_game_action(self):
+        new_game_items = Menu([
+            MenuItem(1, "Reenter name", action.new_game_action),
+            MenuItem(2, "Continue", action.continue_action),
+            MenuItem(3, "Save", None),
+            MenuItem(4, "Quit", action.quit_action),
+            ])
+        new_player.add_character_name()
+        new_game_items.print_input_choose()
+
+action = Actions()
 
 main_menu = Menu([
-        MenuItem(1, "New Game", new_game_action),
+        MenuItem(1, "New Game", action.new_game_action),
         MenuItem(2, "Load Game", None),
         MenuItem(3, "Exit", exit),
     ])
-
 
 new_player = Character()
 monster = Enemy()
