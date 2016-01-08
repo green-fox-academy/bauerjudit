a68d055dff3a94fab40b228c95da5948fc72222d
Judit Bauer <judit.bauer@yahoo.com>
2016-01-08 13:50:39 +0100

    refactoring

--- week-7/gallery/gallery.js
+++ week-7/gallery/gallery.js
@@ -1,44 +1,34 @@
 "use strict";
 
-var title = document.querySelector(".title");
-title.classList.add("title");
-
 var currentPicture = 0;
 
 var images = [
-  "http://desktopography.net/media/exhibition/2010/rush_cocktail/large.jpg",
+  "http://desktopography.net/media/exhibition/2011/flight/large.jpg",
   "http://desktopography.net/media/exhibition/2010/creating_vs_creating/large.jpg",
   "http://desktopography.net/media/exhibition/2010/island_paradise/large.jpg",
   "http://desktopography.net/media/exhibition/2010/spartan/large.jpg"
 ];
 
+var picture = document.querySelector("img");
 var leftClick = document.querySelector(".left");
 var rightClick = document.querySelector(".right");
-
-
-var picture = document.querySelector("img");
-var buttonRight = document.querySelector(".changeright");
-var buttonLeft = document.querySelector(".changeleft");
-
-
 
 
 function nextPicture() {
   if (currentPicture === images.length - 1) {
     currentPicture = 0;
-    changePicture(images[currentPicture]);
   } else {
     currentPicture++;
-    changePicture(images[currentPicture]);
   }
+  changePicture(images[currentPicture]);
 }
 
 function previousPicture() {
   if (currentPicture === 0) {
-    currentPicture = images.length;
-    changePicture(images[currentPicture]);
+    currentPicture = images.length -1;
+  } else {
+    currentPicture--;
   }
-  currentPicture--;
   changePicture(images[currentPicture]);
 }
 
@@ -46,14 +36,6 @@ function changePicture(src) {
   picture.setAttribute("src", src);
 }
 
-
-/*buttonRight.addEventListener("click", function () {
-  nextPicture();
-})
-
-buttonLeft.addEventListener("click", function () {
-  previousPicture();
-})*/
 
 rightClick.addEventListener("click", function () {
   nextPicture();
